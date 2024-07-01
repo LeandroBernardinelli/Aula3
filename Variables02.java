3c5148ce7a07420a60f6668d710fd168cc4c5984
LeandroBernardinelli <151978448+LeandroBernardinelli@users.noreply.github.com>
2024-07-01 00:20:59 -0300

    Add files via upload

---
+++ Variables02.java
@@ -0,0 +1,19 @@
+package Aula3_1;
+
+
+public class Variables02 {
+
+    public static void main(String[] args) {
+        //EXECUTAR O PROGRAMA ENTRE CADA ETAPA E OBSERVAR A SAÍDA
+        //Etapa 1) Altere as instruções de impressão nas linhas 12 e 16 para que imprimam o valor de x.
+        //Etapa 2) Altere o valor de x para "kitty".
+        String x = "kitty";
+        System.out.println(x);
+        x = "bunny";        
+        
+        //Step 3) Add a line of code that changes the value of x to "bunny".
+        
+        System.out.println(x);
+        
+    }
+}
